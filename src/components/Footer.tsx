2fad49ea54db3b7c93a8472ba2702efbe7597139
Luis R Zunniga S <luiscr1104@gmail.com>
2026-01-30 09:02:53 -0600

    seo: optimize meta tags and open graph properties

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { cn } from "../lib/utils";
-import { IconBrandInstagram, IconBrandTwitter, IconBrandFacebook } from "@tabler/icons-react";
 
 export function Footer() {
     return (
